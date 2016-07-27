39748d342af9b89ed549f24664c8e91b98a91b4f
Enrico Mingo <enrico.mingo@iit.it>
2016-07-27 17:09:42 +0200

    Removed operation getJointMappingForPort. Added operation to reset model configuration.

--- include/rtt-gazebo-robot-sim.hpp
+++ include/rtt-gazebo-robot-sim.hpp
@@ -54,14 +54,7 @@ protected:
     std::string printKinematicChainInformation(const std::string& kinematic_chain);
     bool loadURDFAndSRDF(const std::string& URDF_path, const std::string& SRDF_path);
     std::map<std::string, std::vector<std::string> > getKinematiChainsAndJoints();
-
-
-    /**
-     * Provides the joint name to index mapping for other components to retrieve.
-     * If there isn't such an port (portName) existing, or used in an kinematic chain,
-     * the call will return an empty map. Otherwise it will contain the mapping.
-     */
-    std::map<std::string, int> getJointMappingForPort(std::string portName);
+    bool resetModelConfiguration();
 
     gazebo::physics::ModelPtr model;
     gazebo::event::ConnectionPtr world_begin;
--- src/rtt-gazebo-robot-sim.cpp
+++ src/rtt-gazebo-robot-sim.cpp
@@ -45,8 +45,8 @@ robotSim::robotSim(const std::string &name):
     this->addOperation("loadURDFAndSRDF", &robotSim::loadURDFAndSRDF,
                 this, RTT::ClientThread);
 
-    this->provides("joint_info")->addOperation("getJointMappingForPort",
-    			&robotSim::getJointMappingForPort, this, RTT::ClientThread);
+    this->addOperation("reset_model_configuration", &robotSim::resetModelConfiguration,
+                this, RTT::ClientThread);
 
     world_begin = gazebo::event::Events::ConnectWorldUpdateBegin(
             boost::bind(&robotSim::WorldUpdateBegin, this));
@@ -56,35 +56,13 @@ robotSim::robotSim(const std::string &name):
 
 }
 
-std::map<std::string, int> robotSim::getJointMappingForPort(
-		std::string portName) {
-	std::map<std::string, int> result;
-	// find port in kinematic chain. Ports should be unique so no problem here!
-	for (std::map<std::string, boost::shared_ptr<KinematicChain>>::iterator it =
-			kinematic_chains.begin(); it != kinematic_chains.end(); it++) {
-		std::vector<RTT::base::PortInterface*> interface =
-				it->second->getAssociatedPorts();
-		std::vector<base::PortInterface*>::iterator iiter;
-
-		base::PortInterface* candidatePort = 0;
-
-		for (iiter = interface.begin(); iiter != interface.end(); ++iiter) {
-			if ((*iiter)->getName() == portName) {
-				candidatePort = *iiter;
-				break;
-			}
-		}
-
-		if (candidatePort) {
-			std::vector<std::string> jointNames = it->second->getJointNames();
-			// assuming we take the index as stored in the vector...
-			for (unsigned int i = 0; i < jointNames.size(); i++) {
-				result[jointNames[i]] = i;
-			}
-			return result;
-		}
-	}
-	return result;
+bool robotSim::resetModelConfiguration()
+{
+    bool reset = true;
+    std::map<std::string, boost::shared_ptr<KinematicChain>>::iterator it;
+    for(it = kinematic_chains.begin(); it != kinematic_chains.end(); it++)
+        reset = reset && it->second->resetKinematicChain();
+    return reset;
 }
 
 std::string robotSim::printKinematicChainInformation(const std::string& kinematic_chain)
